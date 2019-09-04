9bc25921bdca7f10bd3f299ed7412592472aca4b
AnaiO <anais.berton.io@gmail.com>
2019-09-04 13:32:00 +0200

    CRUD done GroupController

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Users;
+use App\Entity\Groups;
+use App\Form\GroupsType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -21,11 +25,35 @@ class GroupController extends AbstractController
     }
 
     /**
-     * @Route("/group/edit/{user}", name="group_edit", methods={"GET", "POST"})
+     * @Route("/group/edit/{user}/{group}", name="group_edit", methods={"GET", "POST"})
      * @Route("/group/new/{user}", name="group_new", methods={"GET", "POST"})
      */
-    public function new(Users $user)
+    public function new(Users $user, Request $request, ObjectManager $om, Groups $group = null)
     {
+        if (!$group){
+            $group = new Groups();
+        }
+        
+        $form = $this->createForm(GroupsType::class, $group);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $om->persist($group);
+            $om->flush();
+        }
+
+        return $this->render('group/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+        
+    }
+
+    /**
+     * @Route("/delete/{group}", name="group_delete", methods={"DELETE"})
+     */
+    public function delete(Groups $group, ObjectManager $om)
+    {
+        $om->remove($group);
+        $om->flush();
     }
 }
--- src/Form/GroupsType.php
+++ src/Form/GroupsType.php
@@ -6,14 +6,16 @@ use App\Entity\Groups;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class GroupsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
         ;
     }
 
@@ -24,4 +26,5 @@ class GroupsType extends AbstractType
             'novalidate' => 'novalidate'
         ]);
     }
+
 }
